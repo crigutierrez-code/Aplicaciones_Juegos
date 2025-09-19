a1bf20b243376a5ac2e5173835775d9c668ac1cb
Tu Nombre <tu_correo@ejemplo.com>
2025-09-19 15:19:00 -0500

    declaracion de las palabras maximo de fallos y lets a usar

--- ahorcado.js
+++ ahorcado.js
@@ -4,3 +4,10 @@ const msg= document.getElementById("msg");
 const letras= document.getElementById("letras");
 const reset= document.getElementById("reset");
 
+
+const PALABRAS=["javascript","java","python","html","css","react","angular","vue","node","express"];
+const MAX_F =6;
+
+let Palabra, oculta, usadas, fallos;
+
+
