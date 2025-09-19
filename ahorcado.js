bb6917b4e19b35dcb48a89cde0f0cd357ae41b85
Tu Nombre <tu_correo@ejemplo.com>
2025-09-19 15:23:23 -0500

    creacion de la funcion iniciar

--- ahorcado.js
+++ ahorcado.js
@@ -13,3 +13,21 @@ let Palabra, oculta, usadas, fallos;
 function cambiarImagen(fallos){
     img.src = `recursos/ahorcado${fallos}.png`;
 }
+
+function iniciar(){
+    Palabra= PALABRAS[Math.floor(Math.random()*PALABRAS.length)]
+    oculta= Array(Palabra.length).fill("_");
+    usadas= new Set();
+    fallos=0;
+    palabra.textContent= oculta.join(" ");
+    msg.textContent= "";
+    msg.className="";
+    letras.innerHTML="";
+    for(let i=65; i<90; i++){
+        const btn= document.createElement("button");
+        btn.textContent= String.fromCharCode(i);
+        btn.onclick= ()=> manejarLetra(btn.textContent);
+        letras.appendChild(btn);
+    }
+    cambiarImagen(0);
+}
