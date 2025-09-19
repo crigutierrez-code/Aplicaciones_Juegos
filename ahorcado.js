4c93d446da7e72de1b445fa337a1452904c5f8ba
Tu Nombre <tu_correo@ejemplo.com>
2025-09-19 15:16:35 -0500

    creacio del js y declaracion de las constantes

---
+++ ahorcado.js
@@ -0,0 +1,6 @@
+const img= document.getElementById("img");
+const palabra= document.getElementById("Palabra");
+const msg= document.getElementById("msg");
+const letras= document.getElementById("letras");
+const reset= document.getElementById("reset");
+
